6cef4dd0b0ea457651d7563e65fa48e440b0d7f2
natalialopatkina <natalya.lopatkina1994@gmail.com>
2019-10-01 17:38:17 +0300

    Add structure of project

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRouterModule } from './app-router.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRouterModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
