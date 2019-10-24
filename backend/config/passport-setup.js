b0edf3d02453ec43eb783581b02bfd236d8cfce0
natalialopatkina <natalya.lopatkina1994@gmail.com>
2019-10-24 17:30:46 +0300

    Add file keys

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const VKontakteStrategy = require('passport-vkontakte').Strategy;
-// /const keys = require('./keys');
+const keys = require('./keys');
 
 // passport.use(new VKontakteStrategy({
 //     clientID: '',
